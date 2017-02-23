f890380c2da2042f96c93b172ce6f7c8200df8e3
David Knott <david1k1nott@gmail.com>
2017-02-23 09:30:32 -0700

    create item api controller

---
+++ spec/factories.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :item do
+    name "david"
+    description "test"
+    image_url "sup"
+
+  end
+end
+# t.string   "name"
+#     t.text     "description"
+#     t.string   "image_url"
+#     t.datetime "created_at"
+#     t.datetime "updated_at"
--- spec/requests/api/v1/user_can_get_json_of_all_items_spec.rb
+++ spec/requests/api/v1/user_can_get_json_of_all_items_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 describe "When a user sends a GET request to /api/v1/items" do
   scenario "they receive a 200 JSON response containing all items" do
+    create_list(:item, 20)
     get '/api/v1/items'
 
     json_items = JSON.parse(response.body,:symbolize_names => true)
     items = Item.all
-
-    expect(response).to be success
+    expect(response).to be_success
+    expect(json_items.length).to eq items.length
     json_items.each_with_index do |json_item, i|
       expect(json_item[:id]).to eq items[i][:id]
       expect(json_item[:name]).to eq items[i][:name]
