662dc0c99a01c1a5633fb904d97ea03abc1e0472
David Knott <david1k1nott@gmail.com>
2017-02-23 11:41:48 -0700

    refactor service

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,11 +1,11 @@
 class BestBuyService
 
-  def conn
-
+  def self.conn
+    Faraday.new "https://api.bestbuy.com"
   end
 
   def self.search_by(zip)
-    raw_response = Faraday.get "https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&apiKey=#{ENV['BEST_BUY_API_KEY']}"
+    raw_response = conn.get "/v1/stores(area(#{zip},25))", {:format => 'json', :apiKey => ENV['BEST_BUY_API_KEY']}
     JSON.parse(raw_response.body,:symbolize_names => true)
   end
 
