7576c999b34fb2d457eee717b10507ffc7f74200
David Knott <david1k1nott@gmail.com>
2017-02-23 10:31:48 -0700

    implement serializer

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,17 +2,17 @@ class Api::V1::ItemsController < ApplicationController
   before_action :set_item, only: [:show, :destroy]
 
   def index
-    render :json => Item.all, :except => [:created_at, :updated_at]
+    render :json => Item.all
   end
 
   def show
-    render :json => @item, :except => [:created_at, :updated_at]
+    render :json => @item
   end
 
   def create
     item = Item.new(item_params)
     if item.save
-      render :json => item, :status => 201, :except => [:created_at, :updated_at]
+      render :json => item, :status => 201
     end
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,4 @@
 class Item < ActiveRecord::Base
-  include ActiveModel::Serialization
-
   has_many :order_items
   has_many :orders, through: :order_items
 
