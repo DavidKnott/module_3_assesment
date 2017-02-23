cb4f1e508ed5560b5e5ad37c7b799be48c89bf91
David Knott <david1k1nott@gmail.com>
2017-02-23 10:45:17 -0700

    create user can search for stores by zip feature test

---
+++ spec/features/user_can_search_for_stores_by_zip_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "When a user user visits the root path" do
+  scenario "they can search for store by zip" do
+    visit root_path
+
+    fill_in :q, :with => "80202"
+
+    click_on "search"
+
+    expect(page).to have_content "16 Total Stores"
+    expect(page).to have_selector('.store', count: 10)
+    within (".store", match: :first) do
+      expect(page).to have_content "Name: BEST BUY MOBILE - CHERRY CREEK SHOPPING CENTER"
+      expect(page).to have_content "City: DENVER"
+      expect(page).to have_content "Distance: 3.45"
+      expect(page).to have_content "Phone_number: 303-270-9189"
+      expect(page).to have_content "Store_type: Mobile SAS"
+    end
+  end
+end
