c5e0e88fcae5dfab3f4052bd34e6314e784d30b0
David Knott <david1k1nott@gmail.com>
2017-02-23 09:43:37 -0700

    pass items show test

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,8 @@ class Api::V1::ItemsController < ApplicationController
   def index
     render :json => Item.all, :except => [:created_at, :updated_at]
   end
+
+  def show
+    render :json => Item.find(params[:id]), :except => [:created_at, :updated_at]
+  end
 end
--- spec/requests/api/v1/user_can_see_json_of_single_item_spec.rb
+++ spec/requests/api/v1/user_can_see_json_of_single_item_spec.rb
@@ -4,12 +4,12 @@ describe "When a user sends a GET request to /api/v1/items" do
   scenario "they receive a 200 JSON response containing all items" do
     items = create_list(:item, 20)
     item  = items.first
-    get '/api/v1/items/#{item.id}'
+    get "/api/v1/items/#{item.id}"
 
     json_item = JSON.parse(response.body,:symbolize_names => true)
 
     expect(response).to be_success
-    expect(json_item.length).to eq 3
+    expect(json_item.length).to eq 4
     expect(json_item[:id]).to eq item[:id]
     expect(json_item[:name]).to eq item[:name]
     expect(json_item[:description]).to eq item[:description]
