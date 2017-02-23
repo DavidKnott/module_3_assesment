91a16ffaec263f1b8e25c11ec6ec8ee058f71238
David Knott <david1k1nott@gmail.com>
2017-02-23 09:14:18 -0700

    create user can see all items test

---
+++ spec/requests/api/v1/user_can_get_json_of_all_items_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "When a user sends a GET request to /api/v1/items" do
+  scenario "they receive a 200 JSON response containing all items" do
+    get '/api/v1/items'
+
+    json_items = JSON.parse(response.body,:symbolize_names => true)
+    items = Item.all
+
+    expect(response).to be success
+    json_items.each_with_index do |json_item, i|
+      expect(json_item[:id]).to eq items[i][:id]
+      expect(json_item[:name]).to eq items[i][:name]
+      expect(json_item[:description]).to eq items[i][:description]
+      expect(json_item[:image_url]).to eq items[i][:image_url]
+      expect(json_item).to_not have_key :created_at
+      expect(json_item).to_not have_key :updated_at
+    end
+  end
+end
